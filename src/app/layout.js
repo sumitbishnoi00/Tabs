09af15fc74ce83f26b639ee2273d70f24b753bea
sumitbishnoi00 <sumitbishnoi369@gmail.com>
2026-07-14 16:23:18 +0530

    add font-family

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -20,9 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
